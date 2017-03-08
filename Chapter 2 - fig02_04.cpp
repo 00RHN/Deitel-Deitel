c28d974586e73aa8f27cf391a88091ddee868fc7
ORHAN GÜVENÇ <13050161001@ybu.edu.tr>
2017-03-08 20:39:06 +0300

    First three examples belong to chapter 2

---
+++ Chapter 2 - fig02_04.cpp
@@ -0,0 +1,12 @@
+/*Fig. 2.4: fig02_04.c
+  Printing multiple lines with a single printf */
+  
+#include <stdio.h>
+
+/* function main begins program execution */
+int main()
+{
+	printf("Welcome\nto\nC!");
+	
+	return 0; /*indicate that program ended successfully*/
+} /*end function main*/
